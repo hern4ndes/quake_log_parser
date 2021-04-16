2e054d39602834c24a7b65c9aea214ba8f896da2
hern4ndes <erickhernandes4@gmail.com>
2021-04-16 09:12:13 -0300

    encontrar inicio de partida e quem matou, quem morre e motivo localidos via regex

--- quake_log_parser.py
+++ quake_log_parser.py
@@ -2,21 +2,30 @@ import os
 import re
 #
 matou_morreu_causa = r":\s([^:]+)\skilled\s(.*?)\sby\s(.*)"
-comeco_jogo = r" InitGame:"
+comeco_jogo = r"InitGame"
 fim_jogo = r"--------+"
 
 local_path  = os.path.dirname(os.path.abspath(__file__))
 
 with open("quake.log") as log_file:
-    log_file = log_file.read()
-    
-matches = re.finditer(matou_morreu_causa, log_file)
+    log_file = log_file.readlines()
 
-for matchNum, match in enumerate(matches, start=1):
-    
-    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-    print(match.groups())
-    for groupNum in range(0, len(match.groups())):
-        groupNum = groupNum + 1
+game_count = 0 
+
+for i, line in enumerate(log_file,start=1):
+    matches = re.search(comeco_jogo, line)
+    if matches:
+        game_count += 1
+        print("partida = {}".format(game_count))
+   
+    matches = re.search(matou_morreu_causa, line)
+    if matches:
+        print ("Match was found at {start}-{end}: {match}".format(start = matches.start(), end = matches.end(), match = matches.group()))
         
-        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+        for groupNum in range(0, len(matches.groups())):
+            groupNum = groupNum + 1
+            
+            print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = matches.start(groupNum), end = matches.end(groupNum), group = matches.group(groupNum)))
+        
+                
+
